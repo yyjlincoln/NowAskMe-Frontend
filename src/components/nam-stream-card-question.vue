ff3f970ee82a4221774d3b3881d226a9b01b24a7
yyjlincoln <yyjlincoln@gmail.com>
2021-01-22 15:50:56 +1100

    Update: Fixed broken card UI: added text-center

--- src/components/nam-stream-card-question.vue
+++ src/components/nam-stream-card-question.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="w-full h-fit py-10 flex flex-col bg-white rounded-md my-2">
+    <div class="w-full h-fit py-10 flex flex-col bg-white rounded-md my-2 px-10">
       <div class="mx-auto flex flex-row">
         <div class="bg-indigo-400 rounded-md text-white">
           <p class="text-center font-extrabold text-2xl px-2">?</p>
@@ -8,7 +8,7 @@
       </div>
       <div class="mx-auto mt-2 h-full flex flex-row justify-center">
         <span class="text-gray-500">"</span>
-        <p class="whitespace-pre-wrap font-bold">{{ post.content }}</p>
+        <p class="whitespace-pre-wrap font-bold text-center">{{ post.content }}</p>
         <span class="text-gray-500">"</span>
       </div>
       <nam-question-responseinput class="mt-5"></nam-question-responseinput>
